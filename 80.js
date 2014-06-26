3bbf0300eb2d571eb6621eb2e8d72c0b97d1f057
Heming Zhang <x167723@D500845.local>
2014-06-26 16:48:28 -0400

    de casteljau's algorithm

--- 80.js
+++ 80.js
@@ -3,7 +3,7 @@
 // a threshold is {label: "OK", value:0.6, color:"#009900"}
 
 
-function graph(containingElement, grades, barHeight, barWidth){
+function graph(containingElement, grades, barHeight, barWidth, averageLabel){
 	this.containingElement = containingElement;
 	this.element = document.createElement('div');
 	this.element.style.display = "flex";
@@ -20,7 +20,7 @@ function graph(containingElement, grades, barHeight, barWidth){
 
 	average = average / grades.length;
 
-	this.averageBar = new graphBar(average, this.barHeight, this.barWidth * 1.5, "Average");
+	this.averageBar = new graphBar(average, this.barHeight, this.barWidth * 1.5, averageLabel);
 	this.averageBar.element.style.margin = "0 2em 0 1em";
 	this.element.appendChild(this.averageBar.element);
 	this.graphBars = [];
@@ -35,6 +35,7 @@ function graph(containingElement, grades, barHeight, barWidth){
 	this.thresholdContainer = document.createElement('div');
 	this.thresholdContainer.style.position = "absolute";
 	this.thresholdContainer.style.width = this.element.clientWidth + "px";
+	console.log(this.element.clientWidth);
 	this.thresholdContainer.style.height = this.barHeight + "px";
 	this.thresholdContainer.style.left = 0;
 	this.thresholdContainer.style.zIndex = "-10";
@@ -150,3 +151,81 @@ graphBar.prototype.tweenGrade = function(grade) {
 	}).bind(this);
 	f();
 }
+
+function deCasteljau(controlPoints, t){
+	// controlPoints is in the format [[x0, y0], [x1, y1], .. ,[xn, yn]], 0 <= t <=1
+
+	if (controlPoints.length == 1) return controlPoints[0];
+
+	if (controlPoints.length <= 0) {
+		console.log("what the fuck");
+		return;
+	}
+
+	var order = controlPoints.length - 1;
+	var derivedPoints = [];
+	for (var i = 0; i < order; ++i) {
+		derivedPoints[i] = linearInterpolate(controlPoints[i][0],
+			controlPoints[i+1][0], controlPoints[i][1],
+			controlPoints[i+1][1], t);
+	}
+	return deCasteljau(derivedPoints, t);
+}
+
+function linearInterpolate(x1,x2,y1,y2,t) {
+	return [(1-t)*x1 + t*x2, (1-t)*y1 + t*y2];
+}
+
+function waitForWebfonts(fonts, callback) {
+    var loadedFonts = 0;
+    for(var i = 0, l = fonts.length; i < l; ++i) {
+        (function(font) {
+            var node = document.createElement('span');
+            // Characters that vary significantly among different fonts
+            node.innerHTML = 'giItT1WQy@!-/#';
+            // Visible - so we can measure it - but not on the screen
+            node.style.position      = 'absolute';
+            node.style.left          = '-10000px';
+            node.style.top           = '-10000px';
+            // Large font size makes even subtle changes obvious
+            node.style.fontSize      = '300px';
+            // Reset any font properties
+            node.style.fontFamily    = 'sans-serif';
+            node.style.fontVariant   = 'normal';
+            node.style.fontStyle     = 'normal';
+            node.style.fontWeight    = 'normal';
+            node.style.letterSpacing = '0';
+            document.body.appendChild(node);
+
+            // Remember width with no applied web font
+            var width = node.offsetWidth;
+
+            node.style.fontFamily = font;
+
+            var interval;
+            function checkFont() {
+                // Compare current width with original width
+                if(node && node.offsetWidth != width) {
+                    ++loadedFonts;
+                    node.parentNode.removeChild(node);
+                    node = null;
+                }
+
+                // If all fonts have been loaded
+                if(loadedFonts >= fonts.length) {
+                    if(interval) {
+                        clearInterval(interval);
+                    }
+                    if(loadedFonts == fonts.length) {
+                        callback();
+                        return true;
+                    }
+                }
+            };
+
+            if(!checkFont()) {
+                interval = setInterval(checkFont, 50);
+            }
+        })(fonts[i]);
+    }
+};
