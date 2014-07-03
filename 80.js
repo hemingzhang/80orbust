f45cac6e8a476c3e1e809e66ce87097eaf1b603a
Heming Zhang <x167723@D500845.local>
2014-07-03 16:47:37 -0400

    changes in the works

--- 80.js
+++ 80.js
@@ -2,7 +2,6 @@
 // a grade is {value: 0.6, label: "Assignment 1", weight: 1}
 // a threshold is {label: "OK", value:0.6, color:"#009900"}
 
-
 function graphDisplay(header, containingElement, gA) {
 	console.log(gA);
 	this.element = document.createElement('div');
@@ -14,7 +13,7 @@ function graphDisplay(header, containingElement, gA) {
 	this.headerElement.style.marginBottom = "2em";
 	this.element.appendChild(this.headerElement);
 
-	this.graph = new graph(this.element, gA[0], gA[1], gA[2], gA[3]);
+	this.graph = new graph(this.element, gA[0], gA[1], gA[2], gA[3], gA[4]);
 
 	containingElement.appendChild(this.element);
 
@@ -31,7 +30,25 @@ function graphDisplay(header, containingElement, gA) {
 
 }
 
-function graph(containingElement, grades, barHeight, barWidth, averageLabel){
+graphDisplay.prototype.addThresholds = function(thresholdArray, overhang){
+
+	for (i in thresholdArray) {
+		// var element = document.createElement("div");
+		// element.style.position = "absolute";
+		// element.style.height = "1px";
+		// element.style.width = (this.element.clientWidth + 50) + "px";
+		// element.style.right = "0";
+		// element.style.bottom = thresholdArray[i]["value"] * this.barHeight;
+		// element.style.backgroundColor = thresholdArray[i]["color"];
+		// this.thresholdElements[i] = element;
+		// this.thresholdContainer.appendChild(element);
+		this.thresholds.push(new threshold(this.graph.barHeight, this.graph.element.clientWidth + overhang, thresholdArray[i]["label"], thresholdArray[i]["color"], thresholdArray[i]["value"], overhang, this.thresholdContainer));
+	}
+
+	this.graph.element.appendChild(this.thresholdContainer);
+}
+
+function graph(containingElement, grades, barHeight, barWidth, averageLabel, addBarMode){
 	this.containingElement = containingElement;
 	this.element = document.createElement('div');
 	this.element.style.display = "flex";
@@ -42,6 +59,12 @@ function graph(containingElement, grades, barHeight, barWidth, averageLabel){
 	this.barHeight = barHeight;
 	this.barWidth = barWidth;
 
+	this.addBars(addBarMode, grades, barHeight, barWidth, averageLabel);
+
+	this.containingElement.appendChild(this.element);
+}
+
+graph.prototype.addBars = function(mode, grades, barHeight, barWidth, averageLabel){
 	var average = 0;
 	var totalWeight = 0;
 
@@ -52,19 +75,31 @@ function graph(containingElement, grades, barHeight, barWidth, averageLabel){
 
 	average = average / totalWeight;
 
-	this.averageBar = new graphBar(average, this.barHeight, this.barWidth * 1.5, averageLabel, 0);
-	this.averageBar.element.style.margin = "0 2em 0 1em";
-	this.element.appendChild(this.averageBar.element);
-	this.graphBars = [];
-	for (var i = 0; i < grades.length; ++i) {
-		var gB = new graphBar(grades[i]["value"], this.barHeight, this.barWidth, grades[i]["label"], grades[i]["weight"]);
-		this.element.appendChild(gB.element);
-		this.graphBars.push(gB);
+	switch(mode) {
+		case "noAverageTransition":
+			this.averageBar = new graphBar(average, average, this.barHeight, this.barWidth * 1.5, averageLabel, 0);
+			this.averageBar.element.style.margin = "0 2em 0 1em";
+			this.element.appendChild(this.averageBar.element);
+			this.graphBars = [];
+			for (var i = 0; i < grades.length; ++i) {
+				var gB = new graphBar(0, grades[i]["value"], this.barHeight, this.barWidth, grades[i]["label"], grades[i]["weight"]);
+				this.element.appendChild(gB.element);
+				this.graphBars.push(gB);
+			}
+			break;
+
+		default:
+			this.averageBar = new graphBar(0, average, this.barHeight, this.barWidth * 1.5, averageLabel, 0);
+			this.averageBar.element.style.margin = "0 2em 0 1em";
+			this.element.appendChild(this.averageBar.element);
+			this.graphBars = [];
+			for (var i = 0; i < grades.length; ++i) {
+				var gB = new graphBar(0, grades[i]["value"], this.barHeight, this.barWidth, grades[i]["label"], grades[i]["weight"]);
+				this.element.appendChild(gB.element);
+				this.graphBars.push(gB);
+			}
+			break;
 	}
-
-	this.containingElement.appendChild(this.element);
-
-
 }
 
 function threshold(barHeight, width, label, color, value, overhang, containingElement){
@@ -97,25 +132,7 @@ function threshold(barHeight, width, label, color, value, overhang, containingEl
 
 }
 
-graphDisplay.prototype.addThresholds = function(thresholdArray, overhang){
-
-	for (i in thresholdArray) {
-		// var element = document.createElement("div");
-		// element.style.position = "absolute";
-		// element.style.height = "1px";
-		// element.style.width = (this.element.clientWidth + 50) + "px";
-		// element.style.right = "0";
-		// element.style.bottom = thresholdArray[i]["value"] * this.barHeight;
-		// element.style.backgroundColor = thresholdArray[i]["color"];
-		// this.thresholdElements[i] = element;
-		// this.thresholdContainer.appendChild(element);
-		this.thresholds.push(new threshold(this.graph.barHeight, this.graph.element.clientWidth + overhang, thresholdArray[i]["label"], thresholdArray[i]["color"], thresholdArray[i]["value"], overhang, this.thresholdContainer));
-	}
-
-	this.graph.element.appendChild(this.thresholdContainer);
-}
-
-function graphBar(grade, height, width, label, weight) {
+function graphBar(initialGrade, grade, height, width, label, weight) {
 	this.grade = grade;
 	this.label = label;
 	this.weight = weight;
@@ -138,7 +155,20 @@ function graphBar(grade, height, width, label, weight) {
 	// this.barElement.style.height = (this.maxBarHeight * grade).toString() + "px";
 	// this.percentElement.innerText = (this.grade * 100).toFixed(2).toString() + "%";
 
-	this.setGrade(0);
+	this.handleEvent = function(e) {
+		switch(e.type) {
+			case 'transitionend':
+				this.element.removeEventListener('transitionend', this, false);
+				this.element.removeEventListener('webkitTransitionEnd', this, false);
+				break;
+			case 'webkitTransitionEnd':
+				this.element.removeEventListener('transitionend', this, false);
+				this.element.removeEventListener('webkitTransitionEnd', this, false);
+				break;
+		}
+	}
+
+	this.setGrade(initialGrade);
 
 	this.labelElement.innerText = this.label;
 	this.element.appendChild(this.barElement);
@@ -158,17 +188,6 @@ graphBar.prototype.setGrade = function(grade) {
 	this.percentElement.innerText = newGrade + "%";
 }
 
-function getStepFrames(oldGrade, newGrade, frames) {
-	// for now
-	// return [0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,newGrade];
-	var d = awfulBezierApproximation(0.3, 0.4, 0.5, 1, Math.floor(frames * 1.5));
-	var f = normalizePoints(d, frames);
-	var r = [];
-	var delta = newGrade - oldGrade;
-	for(var i = 0; i < f.length; ++i) r[i] = (f[i][1]) * delta + oldGrade;
-	return r;
-}
-
 graphBar.prototype.tweenGrade = function(grade) {
 	var oldGrade = this.grade;
 	var newGrade = grade;
@@ -184,6 +203,23 @@ graphBar.prototype.tweenGrade = function(grade) {
 		}
 	}).bind(this);
 	f();
+}
+
+graphBar.prototype.fadeOut = function(){
+	this.element.className = "fadeOut";
+	this.element.addEventListener("transitionend", this, false);
+	this.element.addEventListener("webkitTransitionEnd", this, false);
+}
+
+function getStepFrames(oldGrade, newGrade, frames) {
+	// for now
+	// return [0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,newGrade];
+	var d = awfulBezierApproximation(0.3, 0.4, 0.5, 1, Math.floor(frames * 1.5));
+	var f = normalizePoints(d, frames);
+	var r = [];
+	var delta = newGrade - oldGrade;
+	for(var i = 0; i < f.length; ++i) r[i] = (f[i][1]) * delta + oldGrade;
+	return r;
 }
 
 function waitForWebfonts(fonts, callback) {
